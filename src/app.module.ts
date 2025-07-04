6dd7d81e6d4871dad99bbbd2973ad8601e863eaf
MauricioLulusis <mauricio.lulusis99@gmail.com>
2025-07-04 16:17:46 -0300

    Configuración cadena de conexión, configuración de modulos y configuración de la entidad

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mssql',
+    host: 'localhost', 
+    port: 1433, 
+    username:'sa', 
+    password:'Nicolas..8', 
+    database:'USER-CRUD',
+    entities:[User],
+    synchronize:true
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
